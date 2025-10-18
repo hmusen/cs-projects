2abb8d954d6e692206b0af361bb8ba363d731783
Ryan <22hajji@uppingham.co.uk>
2025-10-18 16:47:39 +0100

    I added things I learnt from kata

--- unique_in_order.py
+++ unique_in_order.py
@@ -12,3 +12,13 @@ def unique_in_order(sequence):
     print(result)
 
 unique_in_order("ABAAAABBA")
+
+# I may get type errors if I try to access the next element of something that doesn't exist, so go the length and subtract one. 
+
+# In this one, instead of removing from the list itself which will mess up the loop (nono), put the non-duplicated items into a new array just leave out 
+
+# this solution is a bit different. It looks through each element and says is it equal to the next one, if it is, it just skips it instead of appending it, the next iteration will either do the same thing again or have a different i+1, so it will append i. Allowing you to remove all duplicates
+    
+# If you want to turn a string or any data type into a list, use list(variable_to_convert)
+
+# Use my_list[-1] to access the last element of an array. E.g x = [1,23,45,63,"Meow"]. Doing print(x[-1]) will give you the last element, in other words, "meow"
