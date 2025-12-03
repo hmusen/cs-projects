a68cfc99c2e9d56ef1ad2d0de7f363f5499ea895
Ryan <22hajji@uppingham.co.uk>
2025-12-03 06:25:06 +0000

    removed aoc

--- dicts.py
+++
@@ -1,9 +0,0 @@
-students = {
-    'Alice': 85,
-    'Bob': 92,
-    'Charlie': 78,
-    'Diana': 90,
-    'Evan': 88
-}
-
-print([x for x in students if len(x) > 4])
--- size.py
+++
@@ -1,7 +0,0 @@
-from sys import getsizeof
-
-list = []
-
-for i in range(1,100):
-    list.append(i)
-    print(i, getsizeof(list))
